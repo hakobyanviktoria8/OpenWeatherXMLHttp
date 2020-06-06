18b1da9e44c41388ed3c2a7a1c591e358f6da8ce
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-06-07 01:13:46 +0400

    WeatehMap XMLHttpRequest

--- OpenWeatherXHR.js
+++ OpenWeatherXHR.js
@@ -1,6 +1,7 @@
 btn.onclick = function(){
     let city = input.value;
     console.log(city);
+
     let xhr = new XMLHttpRequest();
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial`;
     xhr.open("GET", url, true);
@@ -11,9 +12,8 @@ btn.onclick = function(){
             // console.log(response);
             let results = JSON.parse(response);
             console.log(results);
-            result.innerHTML +=
-                `
-                <div>
+            result.innerHTML =
+                `                
                     <h2>${results.name}</h2>
                     <p>clouds ${results.clouds.all}</p>
                     <p>feels_like ${results.main.feels_like}</p>
@@ -21,10 +21,10 @@ btn.onclick = function(){
                     <p>pressure ${results.main.pressure}</p>
                     <p>temp ${results.main.temp}</p>
                     <p>weather ${results.weather[0].main}</p>
+                    <img src="http://openweathermap.org/img/wn/${results.weather[0].icon}@2x.png" alt="">
                     <p>weather ${results.weather[0].description}</p>
                     <p>wind ${results.wind.speed}</p>
-                </div>   
-                `
+                  `
         }
     };
     xhr.onerror = function() {
